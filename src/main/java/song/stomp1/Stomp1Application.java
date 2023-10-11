cc19948fc0a91c64b1e5378d43b4c8b5d2ec73c1
song windows <dkclasltmf@naver.com>
2023-10-11 23:35:05 +0900

    init project

---
+++ src/main/java/song/stomp1/Stomp1Application.java
@@ -0,0 +1,13 @@
+package song.stomp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stomp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stomp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/song/stomp1/Stomp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package song.stomp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stomp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
