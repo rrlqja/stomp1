cd7af9f57a3cd8f936d478ba0ead62658752d306
song windows <dkclasltmf@naver.com>
2023-10-13 00:03:46 +0900

    add home

---
+++ src/main/java/song/stomp1/controller/HomeController.java
@@ -0,0 +1,19 @@
+package song.stomp1.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome() {
+
+
+        return "home";
+    }
+}
