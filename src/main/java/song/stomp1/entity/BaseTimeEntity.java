c0392f9cf54dafda6ef91c8f7f7e97966c66059e
song windows <dkclasltmf@naver.com>
2023-10-14 15:38:35 +0900

    EntityListener 수정

--- src/main/java/song/stomp1/entity/BaseTimeEntity.java
+++ src/main/java/song/stomp1/entity/BaseTimeEntity.java
@@ -6,12 +6,13 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(EntityListeners.class)
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
     @CreatedDate
