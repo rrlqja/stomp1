c30b185addfd55b5aadd1c3b3af414a1ffd76bfb
song windows <dkclasltmf@naver.com>
2023-10-15 01:00:21 +0900

    findByUsername 추가

--- src/main/java/song/stomp1/repository/UserJpaRepository.java
+++ src/main/java/song/stomp1/repository/UserJpaRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import song.stomp1.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserJpaRepository extends JpaRepository<User, Long> {
-    
+
+    Optional<User> findByUsername(String username);
 }
