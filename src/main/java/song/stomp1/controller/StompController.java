a0b2ce08024018066e9bdabbfddfa6cf1ee15022
song windows <dkclasltmf@naver.com>
2023-10-16 00:57:00 +0900

    stomp 메시지 데이터베이스 저장

--- src/main/java/song/stomp1/controller/StompController.java
+++ src/main/java/song/stomp1/controller/StompController.java
@@ -2,18 +2,38 @@ package song.stomp1.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import song.stomp1.security.UserDetailsImpl;
+import song.stomp1.service.ChatService;
+
+import java.security.Principal;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class StompController {
 
-    @MessageMapping("/chat/{id}")
-    public String chatMessage(@DestinationVariable(value = "id") Long id) {
+    private final ChatService chatService;
 
-        return "hello world!";
+    @MessageMapping("/chat/{chatroomId}")
+    public String chatMessage(@DestinationVariable(value = "chatroomId") Long chatroomId,
+                              Message<String> message,
+                              Principal principal) {
+        log.info("message = {}", message.getPayload());
+
+        UserDetailsImpl userDetails = getUserDetails(principal);
+        Long userId = userDetails.getId();
+
+        String saveMessage = chatService.saveChatting(message, userId, chatroomId);
+
+        return saveMessage;
+    }
+
+    private static UserDetailsImpl getUserDetails(Principal principal) {
+        return (UserDetailsImpl) ((UsernamePasswordAuthenticationToken) principal).getPrincipal();
     }
 }
--- src/main/java/song/stomp1/service/ChatService.java
+++ src/main/java/song/stomp1/service/ChatService.java
@@ -1,11 +1,18 @@
 package song.stomp1.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import song.stomp1.entity.Chat;
+import song.stomp1.entity.Chatroom;
+import song.stomp1.entity.User;
 import song.stomp1.repository.ChatJpaRepository;
+import song.stomp1.repository.ChatroomJpaRepository;
+import song.stomp1.repository.UserJpaRepository;
 
 @Slf4j
 @Service
@@ -13,6 +20,9 @@ import song.stomp1.repository.ChatJpaRepository;
 public class ChatService {
 
     private final ChatJpaRepository chatRepository;
+    private final UserJpaRepository userRepository;
+    private final ChatroomJpaRepository chatroomRepository;
+    private final ObjectMapper objectMapper;
 
     @Transactional
     public Long save(Chat chat) {
@@ -20,5 +30,42 @@ public class ChatService {
         Chat saveChat = chatRepository.save(chat);
 
         return saveChat.getId();
+    }
+
+    @Transactional
+    public String saveChatting(Message<String> message, Long userId, Long chatroomId) {
+        User findUser = getUser(userId);
+        Chatroom findChatroom = getChatroom(chatroomId);
+        String content = getContent(message);
+
+        Chat chat = new Chat();
+        chat.setContent(content);
+        chat.setUser(findUser);
+        chat.setChatroom(findChatroom);
+
+        Chat saveChat = chatRepository.save(chat);
+        log.info("[save chat] chatId = {}, userId = {}, chatroomId = {}", saveChat.getId(), findUser.getId(), findChatroom.getId());
+
+        return saveChat.getContent();
+    }
+
+    private String getContent(Message<String> message) {
+        String content = null;
+
+        try {
+            content = objectMapper.readTree(message.getPayload()).get("content").asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("메시지 파싱 에러");
+        }
+
+        return content;
+    }
+
+    private Chatroom getChatroom(Long chatroomId) {
+        return chatroomRepository.findById(chatroomId).orElseThrow(() -> new RuntimeException("채팅방 없음."));
+    }
+
+    private User getUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new RuntimeException("사용자 없음."));
     }
 }
