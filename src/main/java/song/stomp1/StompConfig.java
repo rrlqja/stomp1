b9927aeab3b72e0134c63dbc5d5ca997f87c9946
song windows <dkclasltmf@naver.com>
2023-10-16 00:59:05 +0900

    stomp authentication 추가

--- src/main/java/song/stomp1/StompConfig.java
+++ src/main/java/song/stomp1/StompConfig.java
@@ -1,15 +1,31 @@
 package song.stomp1;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import song.stomp1.interceptor.InChannelInterceptor;
+
+import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final ApplicationContext context;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -22,8 +38,26 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
                 .enableSimpleBroker("/topic");
     }
 
+
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
+    }
+
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-//        registration.interceptors()
+        AuthorizationManager<Message<?>> authorizationRule = MessageMatcherDelegatingAuthorizationManager.builder()
+                .nullDestMatcher().authenticated()
+                .simpSubscribeDestMatchers("/topic/**").authenticated()
+                .simpDestMatchers("/app/**").authenticated()
+                .anyMessage().denyAll()
+                .build();
+
+        AuthorizationChannelInterceptor authz = new AuthorizationChannelInterceptor(authorizationRule);
+        SpringAuthorizationEventPublisher publisher = new SpringAuthorizationEventPublisher(this.context);
+        authz.setAuthorizationEventPublisher(publisher);
+        registration.interceptors(new SecurityContextChannelInterceptor(), authz
+                , new InChannelInterceptor());
     }
 }
